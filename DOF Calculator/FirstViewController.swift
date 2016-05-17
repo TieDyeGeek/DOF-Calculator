8ccac63e1d7e8d907a4318e3f718cc081a392d99
TieDyeGeek <canderson3.14@me.com>
2016-05-17 12:57:25 -0500

    Initial Commit

---
+++ DOF Calculator/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  DOF Calculator
+//
+//  Created by Cameron Anderson on 5/17/16.
+//  Copyright © 2016 Cameron Anderson. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
