93cdf8c644625a59f922bd0bdb24140a21cc4112
TieDyeGeek <canderson3.14@me.com>
2017-03-30 12:09:25 -0500

    Added Camera Database

--- DOF Calculator/SettingsViewController.swift
+++ DOF Calculator/SettingsViewController.swift
@@ -14,6 +14,8 @@ class SettingsViewController: UIViewController, UIPickerViewDataSource, UIPicker
     
     var firstOpen: Bool?
     var unit: String!
+    var cameraMaker: String!
+    var cameraModel: String!
     var pathToSettings: String!
     var settings: NSMutableDictionary!
     
@@ -62,14 +64,36 @@ class SettingsViewController: UIViewController, UIPickerViewDataSource, UIPicker
         //read settings plist
         settings = NSMutableDictionary(contentsOfFile: pathToSettings!)
         unit = (settings?.value(forKey: "Unit") as? String)!
-
+        cameraMaker = (settings?.value(forKey: "Camera Maker") as? String)!
+        cameraModel = (settings?.value(forKey: "Camera Model") as? String)!
         
-        
+       
         //setup cameraMakeData
+        let fileManager = FileManager.default
+        let bundlePath = Bundle.main.path(forResource: "CameraInfo", ofType: "sqlite")
+        print(bundlePath ?? "No Path -- Error", "\n") //prints the correct path
         
+        
+        let cameraDB = FMDatabase(path: bundlePath!)
+        
+        if (cameraDB?.open())! {
+            let querySQL = "SELECT DISTINCT CameraMaker FROM CameraInfo;"
+            
+            let results:FMResultSet? = cameraDB?.executeQuery(querySQL, withArgumentsIn: nil)
+            
+            while results?.next() == true {
+                cameraMakeData.append((results?.string(forColumn: "CameraMaker"))!)
+            }
+            print("No more camera make data")
+            cameraDB?.close()
+        } else {
+            print("Error: \(String(describing: cameraDB?.lastErrorMessage()))")
+        }
         
         //setup cameraModelData
+        setupCameraModels()
         
+
         
         
         //set delegates and data sources for picker
@@ -83,11 +107,26 @@ class SettingsViewController: UIViewController, UIPickerViewDataSource, UIPicker
         unitsPicker.tag = 1
 
         
-        //TODO
         //set default value for pickers
+        for i in 0 ... cameraMakeData.count-1{
+            if cameraMakeData[i] == cameraMaker {
+                cameraMakeModelPicker.selectRow(i, inComponent: 0, animated: true)
+                print("cameraMake is set to \(i)")
+                break
+            }
+        }
+        
+        for i in 0 ... cameraModelData.count-1{
+            if cameraModelData[i] == cameraModel {
+                cameraMakeModelPicker.selectRow(i, inComponent: 1, animated: true)
+                print("cameraModel is set to \(i)")
+                break
+            }
+        }
+        
         for i in 0 ... unitsData.count-1{ //set unit to current unit
-            print(i)
-            print(unitsData[i])
+            //print(i)
+            //print(unitsData[i])
             if unitsData[i] == unit {
                 unitsPicker.selectRow(i, inComponent: 0, animated: true)
                 print("picker is set to \(i)")
@@ -95,7 +134,38 @@ class SettingsViewController: UIViewController, UIPickerViewDataSource, UIPicker
             }
         }
         
+        cameraMakeModelLabel.text = cameraMaker+" "+cameraModel
+
+    }
+    
+    func setupCameraModels(){
+        let bundlePath = Bundle.main.path(forResource: "CameraInfo", ofType: "sqlite")
+        print(bundlePath ?? "No Path -- Error", "\n") //prints the correct path
+        let cameraDB = FMDatabase(path: bundlePath!)
         
+        if (cameraDB?.open())! {
+            print(cameraMaker)
+            let querySQL = "SELECT CameraModel FROM CameraInfo WHERE CameraMaker = '\(cameraMaker ?? "")';"
+            
+            let results:FMResultSet? = cameraDB?.executeQuery(querySQL, withArgumentsIn: nil)
+            
+            cameraModelData = []
+            while results?.next() == true {
+                cameraModelData.append((results?.string(forColumn: "CameraModel"))!)
+            }
+            print("No more camera model data")
+            cameraDB?.close()
+        } else {
+            print("Error: \(String(describing: cameraDB?.lastErrorMessage()))")
+        }
+        
+        
+        //reset picker to first row
+        cameraMakeModelPicker.selectRow(0, inComponent: 1, animated: true)
+        print("cameraModel is reset to 0")
+        
+        //reset picker data shown
+        cameraMakeModelPicker.reloadComponent(1)
     }
 
     override func didReceiveMemoryWarning() {
@@ -103,6 +173,52 @@ class SettingsViewController: UIViewController, UIPickerViewDataSource, UIPicker
         // Dispose of any resources that can be recreated.
     }
     
+    //updates the sensor info stored in the settings plist
+    func updateSensorInfo(){
+        let bundlePath = Bundle.main.path(forResource: "CameraInfo", ofType: "sqlite")
+        print(bundlePath ?? "No Path -- Error", "\n") //prints the correct path
+        let cameraDB = FMDatabase(path: bundlePath!)
+        
+        if (cameraDB?.open())! {
+            //Sensor width
+            print("SELECT \"SensorWidth(mm)\" FROM CameraInfo WHERE CameraMaker = '\(cameraMaker ?? "")' AND CameraModel = '\(cameraModel ?? "")';")
+            let query1SQL = "SELECT \"SensorWidth(mm)\" FROM CameraInfo WHERE CameraMaker = '\(cameraMaker ?? "")' AND CameraModel = '\(cameraModel ?? "")';"
+            
+            let results1:FMResultSet? = cameraDB?.executeQuery(query1SQL, withArgumentsIn: nil)
+            
+           if results1?.next() == true {
+                let sw = ((results1?.string(forColumn: "SensorWidth(mm)"))!)
+                settings?["Sensor Width"] = Double(sw)
+                settings?.write(toFile: pathToSettings!, atomically: true)
+            } else {
+                print("No sensor data")
+            }
+            print("Sensor Width is \(settings?.value(forKey: "Sensor Width"))")
+
+
+            //sensor Height
+            print("SELECT \"SensorHeight(mm)\" FROM CameraInfo WHERE CameraMaker = '\(cameraMaker ?? "")' AND CameraModel = '\(cameraModel ?? "")';")
+            let query2SQL = "SELECT \"SensorHeight(mm)\" FROM CameraInfo WHERE CameraMaker = '\(cameraMaker ?? "")' AND CameraModel = '\(cameraModel ?? "")';"
+            
+            let results2:FMResultSet? = cameraDB?.executeQuery(query2SQL, withArgumentsIn: nil)
+            
+            if results2?.next() == true {
+                let sh = ((results2?.string(forColumn: "SensorHeight(mm)"))!)
+                settings?["Sensor Height"] = Double(sh)
+                settings?.write(toFile: pathToSettings!, atomically: true)
+                
+            } else {
+                print("No sensor data")
+            }
+            print("Sensor Height is \(settings?.value(forKey: "Sensor Height"))")
+            //print("Sensor Height is "+(settings?.value(forKey: "Sensor Height") as? String)!)
+            
+            cameraDB?.close()
+        } else {
+            print("Error: \(String(describing: cameraDB?.lastErrorMessage()))")
+        }
+
+    }
     
     
     //return number of componetnts (columns) in picker
@@ -133,6 +249,27 @@ class SettingsViewController: UIViewController, UIPickerViewDataSource, UIPicker
         }
     }
     
+    //return width of component
+    func pickerView(_ pickerView: UIPickerView, widthForComponent component: Int) -> CGFloat
+    {
+        let screenSize: CGRect = UIScreen.main.bounds
+        let screenWidth = screenSize.width
+
+        if pickerView.tag == 0 {
+            if component == 0 {
+                return screenWidth * 0.35
+            } else if component == 1 {
+                return screenWidth * 0.5
+            }
+        } else if pickerView.tag == 1 {
+            return screenWidth * 0.45
+        }
+
+        
+        
+        return CGFloat(25.0)
+    }
+    
     //return value of each row in picker
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         if pickerView.tag == 0 {
@@ -150,12 +287,24 @@ class SettingsViewController: UIViewController, UIPickerViewDataSource, UIPicker
     //when pickerView selection changes
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int)  {
         if pickerView.tag == 0{
+            if component == 0 {
+                cameraMaker = cameraMakeData[cameraMakeModelPicker.selectedRow(inComponent: 0)]
+                setupCameraModels()
+            } else if component == 1 {
+                
+            }
+            cameraModel = cameraModelData[cameraMakeModelPicker.selectedRow(inComponent: 1)]
+
             //save camera change to settings plist
-            settings?["Camera Make"] = cameraMakeData[row]
-            settings?["Camera Model"] = cameraModelData[row]
+            settings?["Camera Maker"] = cameraMaker
+            settings?["Camera Model"] = cameraModel
             settings?.write(toFile: pathToSettings!, atomically: true)
-            cameraMakeModelLabel.text = (settings?.value(forKey: "Camera Make") as? String)!+" "+(settings?.value(forKey: "Camera Model") as? String)!
-            print("Camera is "+(settings?.value(forKey: "Camera Make") as? String)!+" "+(settings?.value(forKey: "Camera Model") as? String)!)
+            cameraMakeModelLabel.text = cameraMaker+" "+cameraModel
+            
+            //update sensor info
+            updateSensorInfo()
+            
+            print("Camera is "+(settings?.value(forKey: "Camera Maker") as? String)!+" "+(settings?.value(forKey: "Camera Model") as? String)!)
         } else if pickerView.tag == 1 {
             //save to settings.plist
             settings?["Unit"] = unitsData[row]
